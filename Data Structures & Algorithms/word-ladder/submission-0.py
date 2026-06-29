0ba10a2ebd938b4d9be6656863b218cbc20fc760
Mike Terekhov <113790396+MikeTerekhov@users.noreply.github.com>
2026-06-29 15:39:40 -0400

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # build graph with one letter differences
+        # find shortest path with BFS from start to end word
+        # adj list
+            # hot
+                # *ot -> lines up with 'dot' as well -> save time!
+                # h*t
+                # ho*
+            # [*ot] -> ['hot', 'dot' ...]
+            if endWord not in wordList:
+                return 0
+
+            nei = collections.defaultdict(list)
+            wordList.append(beginWord)
+            for word in wordList:
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j + 1:]
+                    nei[pattern].append(word)
+
+            visit = set([beginWord])
+            q = deque([beginWord])
+            res = 1
+
+            while q:
+                for i in range(len(q)):
+                    word = q.popleft()
+                    if word == endWord:
+                        return res
+                    for j in range(len(word)):
+                        pattern = word[:j] + "*" + word[j + 1:]
+                        for neiWord in nei[pattern]:
+                            if neiWord not in visit:
+                                visit.add(neiWord)
+                                q.append(neiWord)
+
+                res += 1
+            return 0
