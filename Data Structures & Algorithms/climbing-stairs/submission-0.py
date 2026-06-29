89954e57918ead75fc90096a1307e3a9cd5f46fe
Mike Terekhov <113790396+MikeTerekhov@users.noreply.github.com>
2026-06-29 16:33:04 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # last spot in dp array so 1 step
+        one = 1
+        # second to last spot in dp array so
+        two = 1
+        # 2 vars shifted n - 1 times
+
+        # fib type vibe
+            # work backwards to get answer starting from 0
+        for i in range(n - 1):
+            temp = one
+            one  = one + two
+            two = temp
+
+        # this is essentially returning dp[0]
+        return one
